61ff93c54e5a723d26311ce2ee504d5325dfdd98
José Eduardo <jose.eduardo1201@outlook.com>
2021-10-30 18:13:53 -0300

    Service layer, component registration

--- workshop/src/main/java/br/com/workshop/resources/UserResource.java
+++ workshop/src/main/java/br/com/workshop/resources/UserResource.java
@@ -1,19 +1,34 @@
 package br.com.workshop.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.workshop.entities.User;
+import br.com.workshop.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Eduardo", "eduardo@gmail.com", "999999999", "eduardo");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> userList = service.findAll();
+		
+		return ResponseEntity.ok().body(userList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable long id) {
+		User user = service.findById(id);
 		
 		return ResponseEntity.ok().body(user);
 	}
---
+++ workshop/src/main/java/br/com/workshop/services/UserService.java
@@ -0,0 +1,27 @@
+package br.com.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.workshop.entities.User;
+import br.com.workshop.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.get();
+	}
+}
