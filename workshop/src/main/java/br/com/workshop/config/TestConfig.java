5299b691c6990d08576e8df93a27b43b7af46719
José Eduardo <jose.eduardo1201@outlook.com>
2021-11-10 16:04:19 -0300

    Payment, one-to-one association

--- workshop/src/main/java/br/com/workshop/config/TestConfig.java
+++ workshop/src/main/java/br/com/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.workshop.entities.Category;
 import br.com.workshop.entities.Order;
 import br.com.workshop.entities.OrderItem;
+import br.com.workshop.entities.Payment;
 import br.com.workshop.entities.Product;
 import br.com.workshop.entities.User;
 import br.com.workshop.entities.enums.OrderStatus;
@@ -76,5 +77,9 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}	
 }
